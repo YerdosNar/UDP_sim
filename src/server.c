4d1e0556d0b34a34907f802b1e8c9f5572a322bb
YerdosNar <besway2000@gmail.com>
2026-06-22 07:30:51 +0900

    feat(file transfer): Initial structure for file transfer using ACK

--- src/server.c
+++ src/server.c
@@ -1,7 +1,10 @@
 #include "../include/packet.h"
 #include "../include/logging.h"
 #include <arpa/inet.h>
+#include <libgen.h>
 #include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
@@ -9,7 +12,15 @@
 #define PORT            8888
 #define BUFF_LEN        1024
 
-int main(void) {
+int main(int argc, char **argv) {
+        char *filename = argv[1];
+        char *base = basename(filename);
+        u32 namesize = strlen(filename);
+        if (namesize > 0xff) {
+                fprintf(stderr, "ERROR: Why the name is so large?\n");
+                return ERR;
+        }
+
         i32     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
         if (sockfd < 0) {
                 fprintf(stderr, "ERROR: socket() failed\n");
@@ -30,16 +41,18 @@ int main(void) {
         }
 
         packet_t recv_pkt = {0};
-        i32 n = recvfrom(sockfd, (void*)&recv_pkt, MAX_PKT_LEN, MSG_WAITALL,
+        recvfrom(sockfd, (void*)&recv_pkt, MAX_PKT_LEN, MSG_WAITALL,
                         (struct sockaddr *)&cli_addr, &clen);
-        char recv_msg[recv_pkt.header.length];
+        u16 pkt_len = recv_pkt.header.length;
+        char recv_msg[pkt_len + 1];
+        recv_msg[pkt_len] = '\0';
         strncpy(recv_msg, (const char*)recv_pkt.data, sizeof(recv_msg));
         printf("Received: %s\n", recv_msg);
 
         const char *send_msg = "Hello from the Server";
         i8 msg_len = strlen(send_msg);
         packet_t send_pkt = {0};
-        send_pkt.header.seq_id = 0;
+        send_pkt.header.seq_num = 0;
         send_pkt.header.length = msg_len;
         send_pkt.header.type   = DATA;
         memcpy(send_pkt.data, send_msg, msg_len);
@@ -50,6 +63,68 @@ int main(void) {
 
         printf("Sent: %s\n", send_msg);
 
+        FILE *file = fopen(filename, "rb");
+        if (!file) {
+                fprintf(stderr, "ERROR: fopen() failed\n");
+                return ERR;
+        }
+        fseek(file, 0, SEEK_END);
+        u64 fsize = ftell(file);
+        rewind(file);
+
+        packet_t metadata = {0};
+        i32 n = snprintf((char*)metadata.data, MAX_PLD_LEN,
+                        "%s|%lu", base, fsize);
+        metadata.header.type = DATA;
+        metadata.header.length = n;
+        metadata.header.seq_num = 0;
+        sendto(sockfd, (const void*)&metadata,
+               sizeof(header_t) + n,
+               0,
+               (const struct sockaddr*)&cli_addr, clen);
+        printf("Sent metadata: %s\n", metadata.data);
+
+        u64 total_read = 0;
+        u64 seq_num = 0;
+        while (total_read < fsize) {
+                u8 buffer[MAX_PLD_LEN];
+                u16 read_b = fread(buffer, 1, MAX_PLD_LEN, file);
+                if (read_b == 0) {
+                        if (feof(file)) break;
+                        fprintf(stderr, "ERROR: read() failed\n");
+                        return ERR;
+                }
+                total_read += read_b;
+
+                packet_t pkt = {0};
+                pkt.header.type = DATA;
+                pkt.header.seq_num = seq_num;
+                pkt.header.length = read_b;
+                memcpy(pkt.data, buffer, read_b);
+
+                sendto(sockfd, (const void*)&pkt,
+                       pkt.header.length + sizeof(header_t), 0,
+                       (const struct sockaddr*)&cli_addr, clen);
+
+                packet_t ack = {0};
+                recvfrom(sockfd, (void*)&ack, sizeof(packet_t),
+                         0, (struct sockaddr*)&cli_addr, &clen);
+                u64 ack_num = strtoull((char*)ack.data, NULL, 0);
+
+                printf("\rSent: seq_num=%lu, size=%lu, Recv: ack_num=%lu",
+                                seq_num++, total_read, ack_num);
+                fflush(stdout);
+        }
+
+        packet_t pkt_end = {0};
+        pkt_end.header.type = BYE;
+        sendto(sockfd, (const void*)&pkt_end,
+               sizeof(header_t),
+               MSG_CONFIRM,
+               (const struct sockaddr*)&cli_addr, clen);
+        printf("\nSent: BYE\n");
+
+        fclose(file);
         close(sockfd);
         return OK;
 }
